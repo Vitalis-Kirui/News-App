669a8d382f0bfa5ce273933f29ddd61356d912e8
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-19 17:00:36 +0300

    creating routes

--- News-UI/src/app/app-routing.module.ts
+++ News-UI/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { Error404Component } from './Components/error404/error404.component';
 
 const routes: Routes = [
+  {path :'', redirectTo : 'news', pathMatch : 'full'},
+  {path : 'about-us', component : AboutUsComponent},
   { path: 'authentication', loadChildren: () => import('./Modules/authentication/authentication.module').then(m => m.AuthenticationModule) }, 
-  { path: 'news', loadChildren: () => import('./Modules/news/news.module').then(m => m.NewsModule) }];
+  { path: 'news', loadChildren: () => import('./Modules/news/news.module').then(m => m.NewsModule) },
+  {path : '**', component : Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- News-UI/src/app/app.module.ts
+++ News-UI/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { AboutUsComponent } from './Components/about-us/about-us.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { Error404Component } from './Components/error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AboutUsComponent,
-    FooterComponent
+    FooterComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
