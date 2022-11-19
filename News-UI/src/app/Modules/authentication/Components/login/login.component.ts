cf8e03fac6522d909d927e2d90f1b39d7b415084
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-19 17:40:27 +0300

    creating login form

--- News-UI/src/app/Modules/authentication/Components/login/login.component.ts
+++ News-UI/src/app/Modules/authentication/Components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm! : FormGroup;
 
-  ngOnInit(): void {
+  constructor(private service : FormBuilder, private router : Router, private loginService : AuthenticationService) { }
+
+  ngOnInit() {
+
+    this.loginForm = this.service.group({
+      username : ['', [Validators.required]],
+      password : ['', [ Validators.required]]
+    })
+
   }
+
+  // GETTER FUNCTION
+  // Username function
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+    // password function
+    get password() {
+      return this.loginForm.get('password');
+    }
+
+  // login function
+  login(){
+    // this.loginService.login(this.loginForm.value)
+    //     .subscribe(
+    //       data =>{
+    //         localStorage.setItem('token', data.token);
+
+    //         // Navigating to home page after login
+    //         this.router.navigate(['/'])
+    //       },
+    //       error =>{
+    //       console.log(error);
+    //       }
+    //     )
+  }
+
+  // navigating to register page
+  register() {
+    this.router.navigate(['/authentication/register']);
+  };
 
 }
