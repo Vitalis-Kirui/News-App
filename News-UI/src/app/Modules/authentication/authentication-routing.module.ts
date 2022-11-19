c5cb8fa1652bfa885a8e92191f9e39afe6632820
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-19 17:08:34 +0300

    creating individual modules routes

--- News-UI/src/app/Modules/authentication/authentication-routing.module.ts
+++ News-UI/src/app/Modules/authentication/authentication-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication.component';
+import { AccountComponent } from './Components/account/account.component';
+import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
-const routes: Routes = [{ path: '', component: AuthenticationComponent }];
+const routes: Routes = [
+  { path: '', component: AuthenticationComponent },
+  {path : 'login', component: LoginComponent},
+  {path :'register',component : RegisterComponent},
+  {path :'account',component: AccountComponent},
+  {path : 'forget-password', component : ForgetPasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- News-UI/src/app/Modules/news/news-routing.module.ts
+++ News-UI/src/app/Modules/news/news-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessComponent } from './Components/business/business.component';
+import { PoliticsComponent } from './Components/politics/politics.component';
+import { SportsComponent } from './Components/sports/sports.component';
 import { NewsComponent } from './news.component';
 
-const routes: Routes = [{ path: '', component: NewsComponent }];
+const routes: Routes = [
+  { path: '', component: NewsComponent },
+  {path : 'politics', component : PoliticsComponent},
+  {path : 'business', component : BusinessComponent},
+  {path :'sports', component :SportsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
