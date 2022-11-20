4b3ee18e49b0db4c3ce67883826ce16182fd5961
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-20 13:02:41 +0300

    consuming politics endpoint

--- News-UI/src/app/Modules/news/Components/politics/politics.component.ts
+++ News-UI/src/app/Modules/news/Components/politics/politics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/Services/news.service';
 
 @Component({
   selector: 'app-politics',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PoliticsComponent implements OnInit {
 
-  constructor() { }
+  politicsNews : any = [];
 
-  ngOnInit(): void {
+  constructor(private service : NewsService) { }
+
+  ngOnInit(){
+
+    this.service.getPoliticsNews()
+        .subscribe(
+          data =>{
+            this.politicsNews = data.articles;
+          },
+          error =>{
+            console.log(error);
+          }
+        )
+
   }
 
 }
